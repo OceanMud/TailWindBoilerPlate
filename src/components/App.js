479c0e6d8abe7311f4a6aa465a7a9b1aa50b2085
OceanMud <oceanmudhs@gmail.com>
2021-03-29 19:45:21 -0700

    Organized Public

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "../styles/App.css";
 function App() {
   return (
     <div className="App">
-      <div className="text-xl font-bold">hi</div>
+      <div className="text-xl font-bold ">hi</div>
     </div>
   );
 }
